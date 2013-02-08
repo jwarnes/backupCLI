4c32ac5c54fe541764dcbcf9bdc37a42a7e8e154
unknown <jwarnes@.samaritan.org>
2013-02-08 09:27:19 -0500

    changes assembly info

--- BackupMonitorCLI/Properties/AssemblyInfo.cs
+++ BackupMonitorCLI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BackupMonitorCLI")]
+[assembly: AssemblyTitle("SPFieldMonitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BackupMonitorCLI")]
+[assembly: AssemblyCompany("Samaritan's Purse")]
+[assembly: AssemblyProduct("SPFieldMonitor")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
